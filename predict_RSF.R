089a4ffaf3fd66ee2ae1da35f7fdf573c40f1aae
eyma3115 <34910493+eyma3115@users.noreply.github.com>
2020-12-08 16:51:41 +0900

    Delete predict_RSF.R

--- predict_RSF.R
+++
@@ -1,9 +0,0 @@
-library(survival)
-library(randomForestSRC)
-library(MLmetrics)
-
-load(file='RSF_model.rda')
-#Replace the values of the pandas dataframe "random_testset" with real scaled data of the corresponding features
-random_test = read.csv('random_testset.csv', row.names='index')
-
-print(paste0("Predicted comorbidity-free window: ", predict(rf.grow, random_test)$predicted))
