827efee88a8c7665e2e6ff8af5cd9620f20db8cf
eyma3115 <34910493+eyma3115@users.noreply.github.com>
2020-12-17 14:03:20 +0900

    Add files via upload

--- predict_DPGMM.py
+++ predict_DPGMM.py
@@ -12,21 +12,26 @@ with open('PCA_weights.pkl', 'rb') as f:
     pca_weights = pickle.load(f)
 with open('cluster_idx.pkl', 'rb') as f:
     cluster_idx = pickle.load(f)
-#Replace the values of the pandas dataframe "random_testset" with real scaled data of the corresponding features
-random_testset = pd.read_csv('random_testset.csv', index_col='index')
 
-random_testset = np.matmul(random_testset.to_numpy(), np.transpose(pca_weights))
+mean = np.loadtxt('population_mean.csv', delimiter=',')
+std = np.loadtxt('population_std.csv', delimiter=',')
 
-predicted_prob = trained_model.predict_proba(random_testset)[0]
+testset = pd.read_csv('test_input.csv', index_col='index')
+#testset = pd.read_csv('random_testset.csv', index_col='index')
+
+testset = testset.subtract(mean).divide(std)
+testset = np.matmul(testset.to_numpy(), np.transpose(pca_weights))
+
+predicted_prob = trained_model.predict_proba(testset)[0]
 predicted_label = np.argmax([predicted_prob[cluster_idx[0]], predicted_prob[cluster_idx[1]],
                             predicted_prob[cluster_idx[2]], predicted_prob[cluster_idx[3]],
                             predicted_prob[cluster_idx[4]], predicted_prob[cluster_idx[5]]])
 
-print("The predicted assignment probabilities are:")
+print("The predicted cluster assignment probabilities are:")
 print("C1: %2f, C2: %2f, C3: %2f, C4: %2f, C5: %2f, C6: %2f" % (predicted_prob[cluster_idx[0]],
                                                                 predicted_prob[cluster_idx[1]],
                                                                 predicted_prob[cluster_idx[2]],
                                                                 predicted_prob[cluster_idx[3]],
                                                                 predicted_prob[cluster_idx[4]],
                                                                 predicted_prob[cluster_idx[5]]))
-print("Predicted cluster: C%d" % (predicted_label+1))
+print("Predicted phenotype: C%d" % (predicted_label+1))
--- predict_RSF.R
+++ predict_RSF.R
@@ -1,9 +1,26 @@
-library(survival)
 library(randomForestSRC)
-library(MLmetrics)
+library(pec)
 
 load(file='RSF_model.rda')
-#Replace the values of the pandas dataframe "random_testset" with real scaled data of the corresponding features
-random_test = read.csv('random_testset.csv', row.names='index')
 
-print(paste0("Predicted outcome: ", predict(rf.grow, random_test)$predicted))
+testset = read.csv('test_input.csv', row.names='index')
+#testset = read.csv('random_testset.csv', row.names='index')
+
+mean = read.csv('population_mean.csv', header=FALSE)
+std = read.csv('population_std.csv', header=FALSE)
+
+mean = t(mean)
+std = t(std)
+
+testset = (testset-mean)/std
+names(testset) = names(rf.grow$xvar)
+
+prob_5 = predictSurvProb(rf.grow, newdata = testset, times=5)
+prob_10 = predictSurvProb(rf.grow, newdata = testset, times=10)
+prob_15 = predictSurvProb(rf.grow, newdata = testset, times=15)
+
+print(paste0("Predicted 5-year comorbidity-free survival probability: ", prob_5))
+print(paste0("Predicted 10-year comorbidity-free survival probability: ", prob_10))
+print(paste0("Predicted 15-year comorbidity-free survival probability: ", prob_15))
+
+plotPredictSurvProb(rf.grow, newdata=testset, times=0:15)
